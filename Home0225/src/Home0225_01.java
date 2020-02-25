5838e14b37043d75658fc3f8261957a83b8f5b93
yohan <yheez5@naver.com>
2020-02-25 23:46:09 +0900

    Update Home0225_01.java
    두번째 연습 다시 작성함

--- Home0225/src/Home0225_01.java
+++ Home0225/src/Home0225_01.java
@@ -2,81 +2,61 @@
 public class Home0225_01 {
 
 	public static void main(String[] args) {
-		Deck d=new Deck(); 
-		Card[] c=new Card[7];
-		Card c1=d.pick();
-		Card c2=d.pick();
-		Card c3=d.pick(0);
+		Deck d=new Deck();
+		Card[] c1=new Card[7];
+		Card c=d.pick();
+		
 		for(int i=0; i<7;i++) {
-			c[i]=d.pick();
+			c1[i]=d.pick();
 		}
 		for(int i=0; i<7;i++) {
-			System.out.println(c[i]);
+			System.out.println(c1[i]);;
 		}
-
-		System.out.println(c3);
-		System.out.println(c1);
 	}
 
 }
 
-class Deck {
-	final int CARD_MAX = 52;
-	Card[] card_deck = new Card[CARD_MAX];
-
-	Deck() {
-		for (int i = 0; i < Card.KIND_MAX; i++) {
-			for (int j = 0; j < Card.NUM_MAX; j++) {
-				card_deck[i*13+j] = new Card(i + 1, j + 1);
+class Deck{
+	Card[] arr=new Card[Card.MAX_CARD];
+	Deck(){
+		for(int i=0; i<Card.MAX_KIND;i++) {
+			for(int j=0; j<Card.MAX_NUM;j++) {
+				arr[i*13+j]=new Card(i+1,j+1);
 			}
-
 		}
-
 	}
 	Card pick(int index) {
-		return card_deck[index];
+		return arr[index];
 	}
 	Card pick() {
-		int index=(int)(Math.random()*52);
-		return card_deck[index];
+		int random=(int)(Math.random()*52);
+		return arr[random];
 	}
 	void shuffle() {
-		for(int i=0; i<2000;i++) {
-			int num=(int)(Math.random()*52);
-			Card temp=card_deck[0];
-			card_deck[0]=card_deck[num];
-			card_deck[num]=temp;
-		}
+		int random=(int)(Math.random()*52);
+		Card temp=arr[0];
+		arr[0]=arr[random];
+		arr[random]=temp;
 	}
+}
+
+class Card{
+	static final int MAX_CARD=52;
+	static final int MAX_KIND=4;
+	static final int MAX_NUM=13;
 	
-}//class Deck
-
-class Card {
-	static final int KIND_MAX = 4;
-	static final int NUM_MAX = 13;
-
-	static final int SPADE = 4;
-	static final int DIAMOND = 3;
-	static final int HEART = 2;
-	static final int CLOVER = 1;
-
 	int kind;
 	int number;
-
-	Card() {
-
+	Card(){
+		
 	}
-
-	Card(int kind, int number) {
-		this.kind = kind;
-		this.number = number;
+	Card(int kind,int number){
+		this.kind=kind;
+		this.number=number;
 	}
-
 	public String toString() {
-		String[] kinds = { "", "CLOVER", "HEART", "DIAMOND", "SPADE" };
-		String numbers = "0123456789XJQK";
-		return "" + kinds[kind] + " " + numbers.charAt(number);
-
+		String[] kinds= {"","CLOVER","HEART","DIAMOND","SPADE"};
+		String numbers= "0123456789XJQK";
+		return ""+kinds[kind]+" "+numbers.charAt(number);
 	}
-
 }
